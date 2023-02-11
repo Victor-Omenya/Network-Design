b52a603be68261703cbd93da4a4012648becbc6b
Nishch-eye <122944422+Nishch-eye@users.noreply.github.com>
2023-02-11 17:31:34 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,8 @@
 import socket
-import time
 
-IP_ADDRESS = "127.0.0.1"
-PORT = 1200
-BUFFER_SIZE = 1024
+Host = "127.0.0.1"
+Port = 12000
+buff = 1024
 TIMEOUT = 2
 
 # Create a UDP socket
@@ -11,33 +10,34 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 client_socket.settimeout(TIMEOUT)
 
-server_address = (IP_ADDRESS, PORT)
+server_address = (Host, Port)
 client_socket.connect(server_address)
 
 with open("Test_IMG.jpg", "rb") as image_file:
-    # Read the file in chunks
-    chunk = image_file.read(BUFFER_SIZE)
-    while chunk:
-        # Send the chunk to the server
-        sent = client_socket.send(chunk)
+    # Read the file in packets
+    packet = image_file.read(buff)
+    while packet:
+        # Send the packet to the server
+        sent = client_socket.send(packet)
 
         # Wait for ACK from the server
         ack = False
         while not ack:
             try:
                 # Receive the ACK from the server
-                data, address = client_socket.recvfrom(BUFFER_SIZE)
+                data, address = client_socket.recvfrom(buff)
 
                 # Check if the ACK is valid
                 if data == b"ACK":
                     ack = True
             except socket.timeout:
-                # Resend the chunk if ACK is not received in time
-                sent = client_socket.send(chunk)
+                # Resend the packet if ACK is not received in time
+                sent = client_socket.send(packet)
 
-        # Read the next chunk from the file
-        chunk = image_file.read(BUFFER_SIZE)
+        # Read the next packet from the file
+        packet = image_file.read(buff)
 
+print("image was sent and acknowledgement was received")
 # Close the socket
 client_socket.close()
         
