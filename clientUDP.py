0d3de3b5d680b3a2b57b357d0d4194f8d2e18198
Sam <98198530+Samonorom@users.noreply.github.com>
2023-02-12 19:31:33 -0500

    Update clientUDP.py

--- clientUDP.py
+++ clientUDP.py
@@ -1,13 +1,17 @@
+# Import socket and time library
 import socket
 import time
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 8080
-PACKET_SIZE = 2048
-SLEEP_TIME = 1
+# Configuration parameters for client
+UDP_IP = "localhost"      # Could use "127.0.0.1"
+UDP_PORT = 8080           # Assigned port # to communicate
+PACKET_SIZE = 2048        # Size of each packet to send (can be adjusted)
+SLEEP_TIME = 1            # Intervals to prevent overloading
 
+# Create a UDP socket for the client
 Clientsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Uses timeout function from time library to prevent overload
 Clientsock.settimeout(SLEEP_TIME)
 
 # Connect to the server
@@ -15,6 +19,7 @@ server_address = (UDP_IP, UDP_PORT)
 Clientsock.connect(server_address)
 
 # Open the image file for reading
+# Replace "bubbles.jpg" for another file image if desired.
 with open("bubbles.jpg", "rb") as image_file:
     
     # Read the file in Packets
