de6fb2c3389aaa0ad775aee8edf85842194e8c7b
Nishch-eye <122944422+Nishch-eye@users.noreply.github.com>
2023-03-15 21:09:11 -0400

    Update serverUD.py

--- serverUD.py
+++ serverUD.py
@@ -3,29 +3,35 @@ import socket
 
 # Configuration parameters for server
 UDP_IP = 'localhost'   # Could use "127.0.0.1"
-UDP_PORT = 1200        # Assigned port # to communicate
-PACKET_SIZE = 1024     # Size of each packet to receive (can be adjusted)
+UDP_PORT = 8080        # Assigned port # to communicate
+PACKET_SIZE = 2048     # Size of each packet to receive (can be adjusted)
 
-def calculate_checksum(data):
-    checksum1 = 0
-    for i in range(0, len(data), 2):
-        if i + 1 < len(data):
-            checksum1 += (data[i] << 8) + data[i+1]
+def calculate_checksum(packet):
+    checksum = 0
+    for i in range(0, len(packet), 2):
+        if i + 1 < len(packet):
+            w = (ord(packet[i]) << 8) + (ord(packet[i+1]))
+            checksum += w
         else:
-            checksum1 += data[i]
-        checksum1 = (checksum1 & 0xFFFF) + (checksum1 >> 16)
-    return (~checksum1 & 0xFFFF)
+            w = (ord(packet[i]) << 8)
+            checksum += w
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum = checksum + (checksum >> 16)
+    return (~checksum) & 0xffff
 
 # Create a UDP socket for the server
 serversock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to a specific address and port
 serversock.bind((UDP_IP, UDP_PORT))
+print("server is ready to receive the image")
 
 # Open a file to write the image data to
 # Replace "received_image.jpg" for another filename if desired
 with open("received_image.jpg", "wb") as f:
     expected_seq_num = 0
+    received_data = b''
+
     while True:
         # Receive the next packet
         packet, client_address = serversock.recvfrom(PACKET_SIZE)
@@ -37,7 +43,7 @@ with open("received_image.jpg", "wb") as f:
         data = packet_data[2].encode()
         
         # Validate the checksum
-        if calculate_checksum(data) != checksum:
+        if int(calculate_checksum(data)) != checksum:
             print("Packet", seq_num, "has an invalid checksum. Resending ACK", expected_seq_num - 1)
             # Resend the ACK for the previous packet
             ack_packet = str(expected_seq_num - 1).encode()
@@ -49,6 +55,7 @@ with open("received_image.jpg", "wb") as f:
             f.write(data)
             expected_seq_num += 1
             print("Packet", seq_num, "received and written to file.")
+            received_data += data
         
         # Send an ACK for the packet
         ack_packet = str(seq_num).encode()
@@ -57,5 +64,7 @@ with open("received_image.jpg", "wb") as f:
         # If all packets have been received, break out of the loop
         if not packet:
             break
-
+with open("received_image.jpg", 'wb') as f:
+    f.write(received_data)
 print("Image file has been received and written to disk.")
+
